695a1389e0e506a2436bef189937f2789797abfd
vduc301 <127189337+vduc301@users.noreply.github.com>
2023-04-03 08:34:06 +0700

    first

---
+++ GymWeb/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GymWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult feature()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult classes() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+    }
+         public ActionResult Blog() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Single()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
---
+++ GymWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GymWeb.MvcApplication" Language="C#" %>
